76ae6ccfc6aa0f90572919cca75ccf82ba9adf04
cajlakem <39443209+cajlakem@users.noreply.github.com>
2022-02-08 21:20:05 +0100

    rod Settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
+/*
 export const environment = {
   production: true,
   apiLicenseServer: 'rimo-dev.rimo-saas.com',
@@ -6,4 +7,15 @@ export const environment = {
   apiTicketingServer: 'rimo-dev.rimo-saas.com',
   apiTicketingServerToken:
     'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
+}
+*/
+
+export const environment = {
+  production: false,
+  apiLicenseServer: 'license-dev.rimo-systems.eu',
+  apiLicenseServerToken:
+    'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
+  apiTicketingServer: 'rimo-dev2.rimo-saas.com',
+  apiTicketingServerToken:
+    '092701978FDC56534A6560F0A068361686724A91FE83308B362D946D1C7D3203',
 }
