da3be0e048ed1690b5e67a9673067b43163414a7
cajlakem <39443209+cajlakem@users.noreply.github.com>
2022-02-15 12:37:03 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   apiLicenseServer: 'license-dev.rimo-systems.eu',
   apiLicenseServerToken:
     'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
-  apiTicketingServer: 'rimo-dev2.rimo-saas.com',
+  apiTicketingServer: 'rimo-dev.rimo-saas.com',
   apiTicketingServerToken:
-    '092701978FDC56534A6560F0A068361686724A91FE83308B362D946D1C7D3203',
+    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
 }
 
 /*
