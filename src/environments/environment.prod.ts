907cbb3ad2e4a17431a04611d8425e7190cc4f52
cajlakem <39443209+cajlakem@users.noreply.github.com>
2022-03-29 00:49:47 +0200

    token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,10 +14,11 @@ export const environment = {
 
 export const environment = {
   production: false,
-  apiLicenseServer: 'license.rimo-saas.com',
+  mit: 'MIT',
+  apiLicenseServer: 'license-dev.rimo-systems.eu',
   apiLicenseServerToken:
-    '6003512DC5208C1775C7948637D5FACE13E92F047BD3D192E9DA29AABD861FCF',
-  apiTicketingServer: 'rimo-saas.com',
+    'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
+  apiTicketingServer: 'rimo-dev.rimo-saas.com',
   apiTicketingServerToken:
-    'B0CD2A044898950EDBE2A5A110D9F58F665EB7AEFD3FFA928D01D67065ED7B98',
+    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  mit: 'MIT',
   apiLicenseServer: 'license-dev.rimo-systems.eu',
   apiLicenseServerToken:
     'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
@@ -13,9 +14,14 @@ export const environment = {
 }
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+export const environment = {
+  production: false,
+  mit: 'MIT',
+  apiLicenseServer: 'license-dev.rimo-systems.eu',
+  apiLicenseServerToken:
+    'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
+  apiTicketingServer: 'rimo-dev.rimo-saas.com',
+  apiTicketingServerToken:
+    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
+}
  */
