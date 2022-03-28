f4bea2842ad2a991633b66a7fbe959f232b96eb5
cajlakem <39443209+cajlakem@users.noreply.github.com>
2022-03-29 00:24:48 +0200

    token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,23 @@
 /*
 export const environment = {
-  production: true,
-  apiLicenseServer: 'rimo-dev.rimo-saas.com',
+  production: false,
+  apiLicenseServer: 'license.rimo-saas.com',
   apiLicenseServerToken:
-    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
-  apiTicketingServer: 'rimo-dev.rimo-saas.com',
+    '6003512DC5208C1775C7948637D5FACE13E92F047BD3D192E9DA29AABD861FCF',
+  apiTicketingServer: 'rimo-saas.com',
   apiTicketingServerToken:
-    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
+    'B0CD2A044898950EDBE2A5A110D9F58F665EB7AEFD3FFA928D01D67065ED7B98',
 }
+
+
 */
 
 export const environment = {
   production: false,
-  apiLicenseServer: 'license-dev.rimo-systems.eu',
+  apiLicenseServer: 'license.rimo-saas.com',
   apiLicenseServerToken:
-    'E073875D3773662FD86B9FBD2CD2826BEC6F522F20A8A5FD57ACA79762020A41',
-  apiTicketingServer: 'rimo-dev.rimo-saas.com',
+    '6003512DC5208C1775C7948637D5FACE13E92F047BD3D192E9DA29AABD861FCF',
+  apiTicketingServer: 'rimo-saas.com',
   apiTicketingServerToken:
-    'FCD377B4B47F45FC020773BEA1A918A74E303EBAEC3887F1FDB93CD6899EDA57',
+    'B0CD2A044898950EDBE2A5A110D9F58F665EB7AEFD3FFA928D01D67065ED7B98',
 }
